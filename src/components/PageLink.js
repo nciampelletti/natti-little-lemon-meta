79e9e15336eb13ef6231c7b5180eea9b014f5f5c
Natalia Ciampelletti <natashat@shaw.ca>
2024-10-17 16:03:12 -0700

    fixed the pagelink

--- src/components/PageLink.js
+++ src/components/PageLink.js
@@ -1,18 +1,22 @@
+import { Link } from 'react-router-dom'
+
 const PageLink = ({ link, itemClass }) => {
   return (
     <li key={link.id}>
-      <a href={link.href} className={itemClass}>
-        {link.text}
-      </a>
-
-      {/* <a
-        href={link.href}
-        className={itemClass}
-        target={link.href.endsWith('.pdf') ? '_blank' : '_self'}
-        rel="noopener noreferrer"
-      >
-        {link.text}
-      </a> */}
+      {link.href.endsWith('.pdf') ? (
+        <a
+          href={require('../../src/menu.pdf')}
+          target="_blank"
+          rel="noreferrer"
+          className={itemClass}
+        >
+          {link.text}
+        </a>
+      ) : (
+        <Link to={link.href} className={itemClass}>
+          {link.text}
+        </Link>
+      )}
     </li>
   )
 }
